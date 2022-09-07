592427e5f0a2eeb347f9c8cc95575797345aca9e
DESKTOP-4CDEABQ\K-TECH PC <lannd@concrete-corp.com>
2022-09-07 17:09:41 +0700

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -12,14 +12,14 @@
         var self = this
         var ua = navigator.userAgent
         var body = $('body')
-  
+
         if (typeof window.orientation !== 'undefined') {
           $.isMobile = true
           body.addClass('mobile')
         } else {
           body.addClass('pc');
         }
-  
+
         if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0) {
           body.addClass('ios')
         } else if (ua.indexOf('Android') > 0) {
@@ -45,4 +45,30 @@
       }
     }.init())
   })(jQuery)
-  
+
+
+  $(function () {
+    //$('a[href^="#"]').click(function () {
+    $('c-pagetop').click(function () {
+      var speed = 800;
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('html').animate({
+        scrollTop: position
+      }, speed, 'easeInOutCubic');
+      return false;
+    });
+    var $pagetop = $('.c-pagetop');
+    $(window).scroll(function () {
+      if (window.matchMedia('(min-width: 768px)').matches) {
+        if ($(this).scrollTop() > 100) {
+          $pagetop.fadeIn();
+        } else {
+          $pagetop.fadeOut();
+        }
+      } else {
+        $pagetop.fadeOut();
+      }
+    });
+  });
